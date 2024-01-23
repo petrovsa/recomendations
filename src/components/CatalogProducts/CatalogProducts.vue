fe5f73403311a8ed1a450af655e3e6606eb24dd1
petrovsa <petrovsa@ukr.net>
2024-01-23 16:14:18 +0200

    23-01-3

---
+++ src/components/CatalogProducts/CatalogProducts.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="catalog">
+        <ProductsItem :book="product"  v-for="(product, idx) in productsList" :key="idx" />
+    </div>
+</template>
+
+<script>
+import ProductsItem from '@/components/ProductsItem/ProductsItem.vue';
+
+export default {
+    name: 'CatalogProduts',
+
+    props: {
+        productsList: {
+            type: Array,
+            default: () => []
+        }
+    },
+
+    components: {
+        ProductsItem
+    }
+    }
+</script>
+
+<style lang="scss">
+
+.catalog {
+    width: auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto;
+    row-gap: 20px;
+    margin-bottom: 20px;
+
+    @media screen and (min-width: 768px) {
+        max-width: 1700px;
+        grid-template-columns: 1fr 1fr 1fr 1fr;       
+    }
+
+    .product-item {
+      position: relative;
+      max-width: 254px; 
+    }
+}
+</style>
---
+++ src/components/ProductsItem/ProductsItem.vue
@@ -0,0 +1,71 @@
+<template>
+    <a class="product-item" href="#">
+            <img v-if="book.label" class="product-item__label" :src="book.label.url" alt="" />
+            <button :class="['btn', 'product-item__wishlist', {'in-wishlist': book.inWishlist}]" />
+            <button class="btn product-item__cart" />
+            <div class="product-item__imgblock">
+                <img class="product-item__img" :src="book.imgUrl" alt="" />
+            </div>
+            <div class="product-item__text">
+                <div class="product-item__name">
+                    {{ book.name }}
+                </div>
+                <a :href="book.autor.autorUrl" class="product-item__link">
+                    <div class="product-item__autor">
+                        {{ book.autor.autor }}
+                    </div>
+                </a>
+                <div class="product-item__rating rating">
+                    <div class="rating-starslist">
+                        <div class="rating-star">
+                            <img src="./../../assets/star.jpg" alt="star" />
+                        </div>
+                        <div class="rating-star">
+                            <img src="./../../assets/star.jpg" alt="star" />
+                        </div>
+                        <div class="rating-star">
+                            <img src="./../../assets/star.jpg" alt="star" />
+                        </div>
+                        <div class="rating-star">
+                            <img src="./../../assets/star.jpg" alt="star" />
+                        </div>
+                        <div class="rating-star">
+                            <img src="./../../assets/star.jpg" alt="star" />
+                        </div>
+                    </div>
+                    <div class="rating-value">{{ book.ratingLabel }}</div>
+                </div>
+            </div>
+            <div class="product-item__avialability">
+                <div class="product-item__price">
+                    <div :class="['product-item__price-item', {'new': book.oldPrice}]">
+                        <span class="product-item__price-normal">{{ book.price }}</span><span class="product-item__curency-normal">₴</span>
+                    </div>
+                    <div v-if="book.oldPrice" class="product-item__price-item">
+                        <span class="product-item__price-old">{{ book.oldPrice }}</span><span class="product-item__curency-old">₴</span>
+                    </div>
+                </div>
+                <div v-if="book.inStock" class="product-item__status">
+                    В наявності
+                </div>
+                <div v-else class="product-item__status product-item__status--outstock">
+                    Немає в наявності
+                </div>
+            </div>
+        </a>
+</template>
+
+<script>
+export default {
+    name: 'ProductsItem',
+
+    props: {
+        book: {
+            type: Object,
+            default: () => {}
+        }
+    }
+    }
+</script>
+
+<style lang="scss" src="./ProductsItem.scss" />
